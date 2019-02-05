b139c1f8642273edfc775f5ed6efadb1bf45e49a
vargoose84 <vargoose84@gmail.com>
2019-02-05 09:47:52 -0600

    added tutorial like comments

--- routineSelector/src/main/java/edu/stampede3630/routineSelector/App.java
+++ routineSelector/src/main/java/edu/stampede3630/routineSelector/App.java
@@ -7,14 +7,18 @@ import com.google.common.collect.ImmutableList;
 import edu.wpi.first.shuffleboard.api.plugin.Description;
 import edu.wpi.first.shuffleboard.api.plugin.Plugin;
 import edu.wpi.first.shuffleboard.api.widget.ComponentType;
-import edu.wpi.first.shuffleboard.api.widget.WidgetType;;
+import edu.wpi.first.shuffleboard.api.widget.WidgetType;
 
 /**
- * Hello world!
- *
+ * This is the plugin file (notice that it extends Plugin from the shuffleboard API).
+ * The group, name, summary, version are arbitrary, but try to keep the group name for all stampede plugins.
+ * In hindsight, I would have named the "name" DeepSpacePlugins or something to that effect.
  */
 @Description(group = "edu.stampede3630", name = "routineSelector", summary = "My first try at this crap", version = "1.0.0")
 public class App extends Plugin {
+   //this will tell shuffleboard, hey these are all the neat widgets I created
+   //The widgets should be stored in the same folder as this plugin.  This formula should be formatted thusly:
+   // return ImmutableList.of(WidgetType.forAnnotatedWidget(javaWidget.class), WidgetType.forAnnotatedWidget(NEXTJavaWidget.class));
    @Override
    public List<ComponentType> getComponents() {
        return ImmutableList.of(WidgetType.forAnnotatedWidget(RoutineSelectorWidget.class) );
